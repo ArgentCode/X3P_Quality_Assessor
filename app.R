d64b237786342640a85e8193fd93f40f6c8f1942
ArgentCode <craigorman0@gmail.com>
2022-12-21 13:10:57 -0600

    Created a rough draft

---
+++ app.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+predict_one <- function(Forest, X3P) {
+  library(DS401)
+  df <- data.frame(assess_bottomempty = double(),
+                   assess_col_na = double(),
+                   assess_median_na_proportion = double(),
+                   assess_middle_na_proportion = double(),
+                   extract_na = double(),
+                   assess_rotation = double()
+  )
+  df[1,] = c(assess_bottomempty(X3P), assess_col_na(X3P), assess_median_na_proportion(X3P),
+          assess_middle_na_proportion(X3P), extract_na(X3P), assess_rotation(X3P))
+  return(predict(Forest, df, type = 'prob')[,2])
+}
+
+standardQualityForest = get(load("TestForest.RData"))
+library(shiny)
+library(x3ptools)
+options(shiny.maxRequestSize = 15 * 1024^2)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file1", "Choose x3p File", accept = ".x3p"),
+      checkboxInput("header", "Header", TRUE)
+    ),
+    mainPanel(
+      textOutput("prediction")
+    )
+  )
+)
+
+server <- function(input, output) {
+
+
+  observeEvent(input$file1, {
+    file <- input$file1
+    ext <- tools::file_ext(file$datapath)
+    x3p <- read_x3p(file$datapath)
+    pred = as.numeric(predict_one(standardQualityForest, x3p)) * 100
+    output$prediction <- renderPrint({
+      paste("The probability that this is a good scan is", pred, "%")
+    })
+  })
+
+
+}
+
+shinyApp(ui, server)
